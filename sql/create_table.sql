50807d896f04350b07ca0e03e6d7d245fae86919
Anupriya Bhoyar <143840411+anupriyabhoyar@users.noreply.github.com>
2026-05-26 13:54:36 +0530

    Create create_table.sql

---
+++ sql/create_table.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE superstore_db;
+USE superstore_db;
+
+CREATE TABLE superstore (
+    Row_ID INT,
+    Order_ID VARCHAR(20),
+    Order_Date DATE,
+    Ship_Date DATE,
+    Ship_Mode VARCHAR(50),
+    Customer_ID VARCHAR(20),
+    Customer_Name VARCHAR(100),
+    Segment VARCHAR(50),
+    Country VARCHAR(50),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    Postal_Code VARCHAR(20),
+    Region VARCHAR(20),
+    Product_ID VARCHAR(20),
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(255),
+    Sales DECIMAL(10,2),
+    Quantity INT,
+    Discount DECIMAL(5,2),
+    Profit DECIMAL(10,2)
+);
